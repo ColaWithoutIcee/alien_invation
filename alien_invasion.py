c0e63c2df222113b25cc55c046720edb01bafaaf
Jiabing_MacbookPro <Jiabingsun777@gmail.com>
2023-05-21 00:34:17 +0800

    add setting class

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@
 # @Software: PyCharm
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -14,9 +15,12 @@ class AlienInvasion:
     def __init__(self):
         """初始化游戏并创建游戏资源"""
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        # 设置背景色
+        # self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """开始游戏的主循环"""
@@ -25,9 +29,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
-                # 让最近绘制的屏幕可见
-                pygame.display.flip()
+            # 每次循环的时候重新绘制背景色。
+            self.screen.fill(self.settings.bg_color)
+            # 让最近绘制的屏幕可见
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/5/21 00:15
+# @Author  : Jiabing SUN
+# @Email   : jiabingsun777@gmail.com
+# @File    : settings.py
+# @Software: PyCharm
+
+class Settings:
+    # 初始化所有的设置
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+
+        self.bg_color = (230, 230, 230)
+
+
+if __name__ == '__main__':
+    print('Hello World!')
